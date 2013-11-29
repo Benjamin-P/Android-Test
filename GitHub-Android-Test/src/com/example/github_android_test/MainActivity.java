96d90becaa10e59b86ee9fdc8130aaae23c2bfbd
Benjamin-P <bap1989@hotmail.de>
2013-11-29 15:33:41 +0100

    Update MainActivity.java

--- GitHub-Android-Test/src/com/example/github_android_test/MainActivity.java
+++ GitHub-Android-Test/src/com/example/github_android_test/MainActivity.java
@@ -19,5 +19,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+    // Der Kommentar kommt von der GitHub-Seite
 } // ENDE DER KLASSE MainActivity
